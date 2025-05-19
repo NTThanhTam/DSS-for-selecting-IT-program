de00cd9044e3d953ae312f80f1ea65023732d784
NTTThanhTam <thanh19112tam@gmail.com>
2025-05-19 20:48:13 +0700

    Change hard-set session storage for gaining feedback only purpose

--- client/src/App.js
+++ client/src/App.js
@@ -23,21 +23,22 @@ import {
 export const clientURL = `http://localhost:3000`;
 
 const App = () => {
+  sessionStorage.setItem('user', JSON.stringify({ "auth": true, "accessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6InN0dWRlbnQiLCJpYXQiOjE3NDczMDU3NTIsImV4cCI6MTc0NzM5MjE1Mn0.1oKjFXMWuYHATAjiYpydeZ7C4JzA_ArKBd8mRkcXbHA", "Status": "Success", "user_id": 1, "user_role": "student" }))
 
   return (
-    <div  className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen">
       <BrowserRouter>
         <NavBar />
         <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/survey" element={<SurveyPage/>}/>
-            <Route path="/result" element={<Result/>}/>
-            <Route path="/information" element={<Information/>}/>
-            <Route path="/history" element={<HistoryResults/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/survey" element={<SurveyPage />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="/information" element={<Information />} />
+          <Route path="/history" element={<HistoryResults />} />
         </Routes>
-        <Footer className='app'/>
+        <Footer className='app' />
       </BrowserRouter>
     </div>
   );
